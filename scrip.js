663a4ad9d4a2ccf1ce2ef578d6b9371e98ad693b
Abdullah Al Mazid <2008080@ipe.buet.ac.bd>
2025-08-31 16:15:04 +0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,17 @@
+﻿document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.project').forEach(project => {
+    project.addEventListener('mouseenter', () => {
+        project.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+    });
+    project.addEventListener('mouseleave', () => {
+        project.style.boxShadow = 'none';
+    });
+});
